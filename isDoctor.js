dcad796a5e4708a693ff5ba678dd06de9ac33e90
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2025-02-28 12:11:56 +0200

    deleted unnecessary files

--- isDoctor.js
+++
@@ -1,7 +0,0 @@
-// middleware/isDoctor.js
-module.exports = (req, res, next) => {
-    if (req.user.role !== 'doctor') {
-      return res.status(403).json({ message: 'Access denied. You must be a doctor.' });
-    }
-    next(); // Proceed to the next middleware or route handler
-  };
